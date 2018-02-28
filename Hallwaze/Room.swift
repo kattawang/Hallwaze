7eff8cf52525ea21829ff10dccc96e6d705433e1
Katherine Wang (student LM) <s011490@lm-19s011490.lmsd.org>
2018-02-28 11:51:38 -0500

    Room Class

---
+++ Hallwaze/Room.swift
@@ -0,0 +1,23 @@
+//
+//  Room.swift
+//  Hallwaze
+//
+//  Created by Katherine Wang (student LM) on 2/26/18.
+//  Copyright © 2018 Alimah Granieri (student LM). All rights reserved.
+//
+
+import Foundation
+
+class Room{
+    var setTeacher:Dictionary<Int,String>
+    
+    init(setTeachers:[String]){
+        self.setTeacher = Dictionary<Int,String>()
+        for x in 0...8{
+            self.setTeacher[x] = setTeachers[x]
+        
+        }
+    }
+    
+    
+}
