d8b71a33c366bb256ff54c36bf85deb2032e2a78
Alimah Granieri (student LM) <s011744@lm-19s011744.lmsd.org>
2018-02-28 12:05:00 -0500

    Master class

---
+++ Hallwaze/Master.swift
@@ -0,0 +1,21 @@
+//
+//  Master.swift
+//  Hallwaze
+//
+//  Created by Alimah Granieri (student LM) on 2/28/18.
+//  Copyright © 2018 Alimah Granieri (student LM). All rights reserved.
+//
+
+import Foundation
+
+class Master{
+    var rooms: [Int:Room]
+    
+    init(roomNumber: Int, teacher: [Room]){
+        rooms = [:]
+        for set in 0...8{
+            self.rooms[roomNumber] = teacher[set]
+        }
+    }
+    
+}
