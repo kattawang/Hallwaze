ccfe0ad7058f99ce6591b97a10a1bca7b5b59fff
Katherine Wang (student LM) <s011490@lm-19s011490.lmsd.org>
2018-04-05 13:43:39 -0400

    master schedule text box outlets

--- Hallwaze/ViewController.swift
+++ Hallwaze/ViewController.swift
@@ -10,10 +10,81 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func a1(_ sender: UITextField) {
+    }
+    
+    @IBAction func a2(_ sender: UITextField) {
+    }
+    
+    @IBAction func a3(_ sender: UITextField) {
+    }
+    
+    @IBAction func a5(_ sender: UITextField) {
+    }
+    
+    @IBAction func a6(_ sender: UITextField) {
+    }
+    
+    @IBAction func a7(_ sender: UITextField) {
+    }
+    
+    @IBAction func b4(_ sender: UITextField) {
+    }
+    
+    @IBAction func b1(_ sender: UITextField) {
+    }
+    
+    @IBAction func b2(_ sender: UITextField) {
+    }
+    
+    @IBAction func b8(_ sender: UITextField) {
+    }
+    
+    @IBAction func b5(_ sender: UITextField) {
+    }
+    
+    @IBAction func b6(_ sender: UITextField) {
+    }
+    
+    @IBAction func c3(_ sender: UITextField) {
+    }
+    
+    @IBAction func c4(_ sender: UITextField) {
+    }
+    
+    @IBAction func c1(_ sender: UITextField) {
+    }
+    
+    @IBAction func c7(_ sender: UITextField) {
+    }
+    
+    @IBAction func c8(_ sender: UITextField) {
+    }
+    
+    @IBAction func c5(_ sender: UITextField) {
+    }
+    
+    @IBAction func d2(_ sender: UITextField) {
+    }
+    
+    @IBAction func d3(_ sender: UITextField) {
+    }
+    
+    @IBAction func d4(_ sender: UITextField) {
+    }
+    
+    @IBAction func d6(_ sender: UITextField) {
+    }
+    
+    @IBAction func d7(_ sender: UITextField) {
+    }
+    
+    @IBAction func d8(_ sender: UITextField) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("hello world")
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
